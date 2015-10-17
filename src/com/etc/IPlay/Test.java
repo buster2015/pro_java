3ecfd74b22aa7bf6b321a56768eaa26b1930fdb3
eyes <guohongyan1991@163.com>
2015-10-17 18:10:37 +0800

    测试类
    Signed-off-by: eyes <guohongyan1991@163.com>

---
+++ src/com/etc/IPlay/Test.java
@@ -0,0 +1,7 @@
+package com.etc.IPlay;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("lllll");
+	}
+}
--- src/pro_java/TestTest.java
+++
@@ -1,7 +0,0 @@
-package pro_java;
-
-public class TestTest {
-	public static void main(String[] args) {
-		System.out.println("fdafdafda");
-	}
-}
